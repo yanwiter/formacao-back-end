c90c4c5fb99dfd9e79f28eef9e40a58092b66257
Yan Witer <yanwiter@gmail.com>
2023-04-08 00:30:45 -0300

    Initial commit

---
+++ .Net/Windows Forms/HelloWorld/HelloWorld/HelloWorld.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace HelloWorld
+{
+    partial class HelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(224, 189);
+            label1.Name = "label1";
+            label1.Size = new Size(322, 65);
+            label1.TabIndex = 0;
+            label1.Text = "Hello World!!!";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(664, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Fechar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // HelloWorld
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "HelloWorld";
+            StartPosition = FormStartPosition.CenterScreen;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+    }
+}
